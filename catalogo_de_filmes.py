0b35f6a4a54721be3d5da171ef08c87ba1758cdc
César Oliveira <73895379+CesarOli@users.noreply.github.com>
2023-06-06 23:49:18 -0300

    Adicionar classe Filme com atributos.

---
+++ catalogo_de_filmes.py
@@ -0,0 +1,6 @@
+class Filme:
+    def __init__(self, titulo, diretor, genero, ano_lancamento):
+        self.titulo = titulo
+        self.diretor = diretor
+        self.genero = genero
+        self.ano_lancamento = ano_lancamento
