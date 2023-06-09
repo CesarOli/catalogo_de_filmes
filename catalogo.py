69c9e41f6289607f313f3a6261b221b305b6d6c7
César Oliveira <73895379+CesarOli@users.noreply.github.com>
2023-06-08 23:41:12 -0300

    Atualização no catalogo de filmes.

--- catalogo.py
+++ catalogo.py
@@ -1,9 +1,25 @@
 from filme import Filme
 
+'''Lista de Filmes
+'Rocky', 'John G. Avildsen', 'Drama', 1976
+'Rocky II', 'Sylvester Stallone', 'Drama', 1979
+'Rocky III', 'Sylvester Stallone', 'Drama', 1982
+'Rocky IV', 'Sylvester Stallone', 'Drama', 1985
+'Rocky V', 'John G. Avildsen', 'Drama', 1990
+'Rocky Balboa', 'Sylvester Stallone', 'Drama', 2006
+'Creed', 'Ryan Coogler', 'Drama', 2015
+'Creed II', 'Steven Caple Jr.', 'Drama', 2018
+'''
+
 catalogoDeFilmes = { 
     
     #chave    classe e informações dos filmes       
-    'filme1': Filme('Filme 1', 'Diretor 1', 'Gênero 1', 2021),
-    # procurar uma lista de filmes (filmes que inspiram)
-
+    'filme1': Filme('Rocky', 'John G. Avildsen', 'Drama', 1976),
+    'filme2': Filme('Rocky II', 'Sylvester Stallone', 'Drama', 1979),
+    'filme3': Filme('Rocky III', 'Sylvester Stallone', 'Drama', 1982),
+    'filme4': Filme('Rocky IV', 'Sylvester Stallone', 'Drama', 1985),
+    'filme5': Filme('Rocky V', 'John G. Avildsen', 'Drama', 1990),
+    'filme6': Filme('Rocky Balboa', 'Sylvester Stallone', 'Drama', 2006),
+    'filme7': Filme('Creed', 'Ryan Coogler', 'Drama', 2015),
+    'filme8': Filme('Creed II', 'Steven Caple Jr.', 'Drama', 2018),
 }
