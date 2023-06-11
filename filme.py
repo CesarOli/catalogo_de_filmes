c49c7d90c056fbed45759c6f5dc7eb503cf7f16e
César Oliveira <73895379+CesarOli@users.noreply.github.com>
2023-06-10 23:52:25 -0300

    Atualização de arquivos e criação da função editaFilmes e sua estrutura.

--- filme.py
+++ filme.py
@@ -1,3 +1,5 @@
+from time import sleep
+
 class Filme:
     def __init__(self, titulo, diretor, genero, ano_lancamento):
         self.titulo = titulo
@@ -5,4 +7,30 @@ class Filme:
         self.genero = genero
         self.ano_lancamento = ano_lancamento
 
+    def imprimirNomeFilme(self):
+        return self.titulo
+
+catalogoDeFilmes = { 
+    
+    #chave    classe e informações dos filmes       
+    'filme1': Filme('Rocky', 'John G. Avildsen', 'Drama', 1976),
+    'filme2': Filme('Rocky II', 'Sylvester Stallone', 'Drama', 1979),
+    'filme3': Filme('Rocky III', 'Sylvester Stallone', 'Drama', 1982),
+    'filme4': Filme('Rocky IV', 'Sylvester Stallone', 'Drama', 1985),
+    'filme5': Filme('Rocky V', 'John G. Avildsen', 'Drama', 1990),
+    'filme6': Filme('Rocky Balboa', 'Sylvester Stallone', 'Drama', 2006),
+    'filme7': Filme('Creed', 'Ryan Coogler', 'Drama', 2015),
+    'filme8': Filme('Creed II', 'Steven Caple Jr.', 'Drama', 2018),
+}
+for chave in catalogoDeFilmes:
+    filme = catalogoDeFilmes[chave]
+    sleep(0.4)
+    print(filme.titulo)
+sleep(1.5)
+print('Fim do programa!!')
+
+
+def editarFilmes(catalogo, titulo):
+    if:
+    else:
         
